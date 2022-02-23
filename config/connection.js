42b121729e500509a9f184266f01fd1c6f78cfd8
mahroofalipadinjarayil <alimahroof155@gmail.com>
2022-02-24 00:44:51 +0530

    7th version

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,24 @@
- const mongoclient = require('mongodb').MongoClient
-     // const { MongoClient } = require("mongodb").MongoClient;
- const state = {
-     db: null
- }
+const mongoclient = require('mongodb').MongoClient
+require('dotenv').config();
 
- module.exports.connect = function(done) {
-     const url = 'mongodb://localhost:27017'
-     const dbname = 'Electra'
-         //create connection
-     mongoclient.connect(url,{ useUnifiedTopology: true }, (err, data) => {
-         if (err)
-             return done(err)
-         state.db = data.db(dbname)
-         done()
-     })
+const { MongoClient } = require("mongodb").MongoClient;
+const state = {
+    db: null
+}
+const pass = process.env.PASSWORD_DB
+const dbname = process.env.DB_NAME
+module.exports.connect = function (done) {
+    const url = `mongodb+srv://mahroofali:${pass}@cluster0.omtn0.mongodb.net/${dbname}?retryWrites=true&w=majority`
+    //create connection
+    mongoclient.connect(url, { useUnifiedTopology: true }, (err, data) => {
+        if (err)
+            return done(err)
+        state.db = data.db(dbname)
+        done()
+    })
 
+}
 
- }
-
- module.exports.get = function() {
-     return state.db
- }
+module.exports.get = function () {
+    return state.db
+}
